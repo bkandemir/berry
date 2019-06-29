f070a5e791f6fe4516fc636b7e2713cea2c64f9c
berkaykandemir <berkaykandemir@10.50.70.20>
2019-06-29 10:05:02 +0300

    First Commit

---
+++ berry/src/main/java/edu/sabanciuniv/berry/BerryApplication.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.berry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerryApplication.class, args);
+	}
+
+}
