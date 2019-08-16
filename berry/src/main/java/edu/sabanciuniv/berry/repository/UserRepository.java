b157695f7aa1c9f2d53de585da831ec9131d3280
berkaykandemir <berkaykandemir@192.168.1.28>
2019-08-17 00:33:28 +0300

    User Search and User Profiles implemented

--- berry/src/main/java/edu/sabanciuniv/berry/repository/UserRepository.java
+++ berry/src/main/java/edu/sabanciuniv/berry/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package edu.sabanciuniv.berry.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import edu.sabanciuniv.berry.domain.User;
 
 public interface UserRepository extends CrudRepository<User, String> {
+	
+	@Query("SELECT u FROM User u where username like %:username%") 
+    List<User> findByUserNameContaining(@Param("username") String username);
 
 }
